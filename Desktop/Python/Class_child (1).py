a3bb40ed64789c04468a298be26b125143b5154e
Miguelan3l <miguelangel.montelongo.7@gmail.com>
2020-09-08 20:05:33 -0600

    Add files via upload

---
+++ Desktop/Python/Class_child (1).py
@@ -0,0 +1,16 @@
+class User:
+    name = 'No Name Provided'
+    email = ' '
+    password = '1234abc'
+    account_number = 0
+class Emloyee(User):
+    name = 'Jerry'
+    email = 'Jerry123@yahoo.com'
+    password = "123ew"
+    account_number = '33'  
+class Customer(User):
+    name = 'Mary'
+    email = 'mary33@yahoo.com
+    password = '4456pass'
+    account_number = '44'
+   
