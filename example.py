2e429167311cefb197c6756e9288db1a9a4d82a8
Miguelan3l <miguelangel.montelongo.7@gmail.com>
2020-09-28 21:18:40 -0600

    Add files via upload

---
+++ example.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import webbrowser
+
+def wbbrowser():
+    f = open('index.html','w')
+    message = "<html><head></head><body><p>%s</p></body</html>"
+    f.write(message)
+    f.close()
+    webbrowser.open_new_tab('index.html')
+   
+
+wbGui = Tk()
+source = StringVar()
+
+
+wbGui.geometry('450x450+500+300')
+wbGui.title('Web Browser')
+
+wblabel = Label(wbGui,text='Type Your Text Below').pack()
+
+wbbutton = Button(wbGui,text="Open Browser",command = wbbrowser).pack()
+
+wbEntry = Entry(wbGui,textvariable=source).pack()
+
+
+def wbbrowser():
+    
+    text = source.get()
+    message = "<html><head></head><body><p>%s</p></body</html>" % text
+    
